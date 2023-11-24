d6705eabeba95d4f1e93d5e06731012c0298d0c4
Perla118 <perlamaria9545@gmail.com>
2023-11-24 08:57:50 -0700

    first commit

---
+++ Lista.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenU3
+{
+    public class Lista
+    {
+        Nodo primero;
+        Nodo ultimo;
+
+        public bool ListaVacia()
+        {
+           if(primero== null)
+            {
+                return true;
+            }
+           return false;
+        }
+        public void InsertarPersona()
+        {
+            Console.WriteLine("Ingrese el nombre");
+            string nuevoNombre = Console.ReadLine();
+            Console.WriteLine("Ingrese la edad");
+            int nuevoEdad = Convert.ToInt32(Console.ReadLine);
+
+            Persona nuevoPersona = new Persona(nuevoNombre,nuevoEdad);
+            Nodo nuevoNodo = new Nodo(nuevoPersona);
+
+            if(ListaVacia())
+            {
+                Console.WriteLine("La lista esta vacía");
+            }
+         
+            else
+            {
+                Nodo actual = primero;
+                while (actual.getSiguiente() != null)
+                {
+                    actual = actual.getSiguiente();
+                }
+
+            
+            }
+        }
+    }
+}
---
+++ Persona.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenU3
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+
+
+        public Persona(string nombre, int edad)
+        {
+            Nombre = nombre;
+            Edad = edad;
+        }
+    }
+}
